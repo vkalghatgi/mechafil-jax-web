83d3595ddeb21ce35b72140807f55ac0180e4a6a
Kiran Karra <kiran.karra@gmail.com>
2023-07-24 21:27:34 -0700

    trying bugfix for update

--- mechafil_jax_web/main.py
+++ mechafil_jax_web/main.py
@@ -86,7 +86,7 @@ def plot_panel(results, baseline, start_date, current_date, end_date):
                     ],)
             .properties(title="1Y Sector FoFR")
             .configure_title(fontSize=14, anchor='middle')
-            .add_selection(hover)
+            .add_params(hover)
         )
         # tooltip = (
         #     alt.Chart(roi_df)
